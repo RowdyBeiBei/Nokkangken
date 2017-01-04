f8a8ffcda038dcd273b4eb6ae167128107a93d27
Nicholas W Fortner <nwfortner@gmail.com>
2017-01-04 11:12:22 -0800

    [refactor/add](client) add TimeSelector component & refactor use momentJS {nwf}

---
+++ client/modules/timeSelector/TimeSelector.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import moment from 'moment';
+
+
+
+class TimeSelector extends React.Component {
+
+  renderOptionsElements() {
+    // console.log(moment().startOf('day').format('LT'));
+    const hoursInDay = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+    return hoursInDay.map((hour, index) => {
+      return (
+        <option key={index} value={moment().startOf('Day').add(hour, 'hours')}>
+          {moment().startOf('Day').add(hour, 'hours').format('LT')}
+        </option>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <select onChange={(event) => {this.props.setTimePreferance(event.target.value);}}>
+        {this.renderOptionsElements()}
+      </select>
+    );
+  }
+}
+
+
+export default TimeSelector;
