39b088fcf518d854c2c4f00ab63c0fc1e888a075
Dan Gilday <dangitday@gmail.com>
2017-01-07 16:00:13 -0800

    [add](sql)Add sql query to check for matches{dvg}

---
+++ database/sql/users/checkMatch.sql
@@ -0,0 +1,18 @@
+
+
+SELECT u.name, u.id, r2.id_user, r2.wouldjoin, r1.id_user, r2.id_prospect, r1.wouldjoin
+FROM users as u
+INNER JOIN responses as r1
+  ON u.id = r1.id_prospect
+INNER JOIN responses as r2
+  ON r2.id_prospect = r1.id_user
+    AND r1.id_prospect = r2.id_user
+INNER JOIN possibles as p1
+  ON p1.id_user = r1.id_user
+INNER JOIN possibles as p2
+  ON p2.id_user = r2.id_user 
+WHERE r1.wouldjoin = TRUE
+AND r2.wouldjoin = TRUE
+AND p1.possibleTime = p2.possibleTime
+AND r1.id_user = ${userId}
+AND p1.possibleTime = ${eventTime};
