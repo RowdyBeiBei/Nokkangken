3b1cc98d8c28aefff6db161abfb1c3b2761d7afc
BettyBeiZhang <beizhang18@yahoo.com>
2017-01-12 11:21:09 -0800

    landing page rebuided UI

--- client/modules/landing/Landing.jsx
+++ client/modules/landing/Landing.jsx
@@ -6,12 +6,20 @@ class Landing extends React.Component {
 	  render() {
 		   return (
 				 <div>
-		  	 	<div className='container hero-container'>
-	 					<h1 className='header'> Nokkangken </h1>
-	 					<p className='message'> Meet someone with the chance created by yourself</p>
-						<Link to='signup'><button className='btn btn-primary pull-right mb2'>SignUp</button></Link>
-	 					<Link to='login'><button className='btn btn-primary pull-right mb2 mr2'>Log In</button></Link>
+		  	 	<div className='container'>
+						<h1 className='logo pull-left'>Nokkangken</h1>
+						<Link to='login'><button className='btn btn-primary pull-right mt2'>Log In</button></Link>
+						<div className="hero-container text-center">
+							<h2 className='header'>Meet someone today!</h2>
+		 					<p className='message'>It's easy, just choose your your interests and pick a time. And
+							we'll setup matches for you.
+							</p>
+							<Link to='signup'><button className='btn btn-primary mx-auto'>Get Started</button></Link>
+
+						</div>
+
 			  	</div>
+
 					{this.props.children}
 				 </div>
 
