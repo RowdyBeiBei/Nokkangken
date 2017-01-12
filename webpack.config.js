ec47498250dff5deba76592b78fca809b34647b4
Nicholas W Fortner <nwfortner@gmail.com>
2017-01-12 09:36:18 -0800

    [reconfigure](webpack) webpack production configuration setup {nwf}

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports= {
-  devtool: '#source-map',
+  devtool: 'cheap-module-source-map',
   entry: [
     'webpack/hot/dev-server',
     'webpack-hot-middleware/client',
@@ -28,7 +29,18 @@ module.exports= {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new HTMLWebpackPlugin()
   ],
   target: 'web'
 };
