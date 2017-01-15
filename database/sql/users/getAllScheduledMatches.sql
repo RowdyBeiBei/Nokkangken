a9baf33d3bb85480739116e86594f32d4e639757
Dan Gilday <dangitday@gmail.com>
2017-01-15 15:17:06 -0800

    [update](db)update scheduled match query{dvg}

--- database/sql/users/getAllScheduledMatches.sql
+++ database/sql/users/getAllScheduledMatches.sql
@@ -1,6 +1,6 @@
 --should take input ${userId} and output user info for all users that have schedulded events
 
-SELECT * FROM users As u 
+SELECT u.id, u.facebook_id, u.name, u.email, u.bio, u.picture, us1.id_scheduled FROM users As u 
 INNER JOIN users_scheduleds AS us1
   ON u.id = us1.id_user
 WHERE us1.id_scheduled = 
