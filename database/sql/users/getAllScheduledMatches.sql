a3222ebf4962171e1fa1b0f2e26529df878a4c98
Dan Gilday <dangitday@gmail.com>
2017-01-16 08:39:01 -0800

    [update](query)Get more info scheduleds{dvg}

--- database/sql/users/getAllScheduledMatches.sql
+++ database/sql/users/getAllScheduledMatches.sql
@@ -1,8 +1,10 @@
 --should take input ${userId} and output user info for all users that have schedulded events
 
-SELECT u.idu, u.facebook_id, u.name, u.email, u.bio, u.picture, us1.id_scheduled FROM users As u 
+SELECT u.idu, u.facebook_id, u.name, u.email, u.bio, u.picture, us1.id_scheduled, s.scheduledTime, s.businessId FROM users As u 
 INNER JOIN users_scheduleds AS us1
   ON u.idu = us1.id_user
+INNER JOIN scheduleds AS s
+  ON us1.id_scheduled = s.ids
 WHERE us1.id_scheduled = 
   ANY (SELECT s.ids FROM scheduleds AS s
     INNER JOIN users_scheduleds AS us2
